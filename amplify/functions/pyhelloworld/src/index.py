e1f48aef5e5165610ae85ccef8dca20f2d676519
kmcclusk-ux <kmcclusk@gmail.com>
2026-01-22 15:41:08 -0800

    Add Lambda handler with CORS support

--- amplify/functions/pyhelloworld/src/index.py
+++ amplify/functions/pyhelloworld/src/index.py
@@ -1,1 +1,13 @@
 
+import json
+
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps('Hello from Python Lambda!')
+    }
