6d860da05534d5b020ee58ae33df16b5ef3ae058
kmcclusk-ux <kmcclusk@gmail.com>
2026-01-25 08:51:02 -0800

    Add tax calculation Lambda function

---
+++ amplify/functions/calc-tax/index.py
@@ -0,0 +1,11 @@
+import json
+
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*' 
+        },
+        'body': json.dumps("hey go let's go")
+    }
