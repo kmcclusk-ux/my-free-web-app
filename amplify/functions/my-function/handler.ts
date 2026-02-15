1f84d3f340f5fe0203216121807f04b20428774e
kmcclusk-ux <kmcclusk@gmail.com>
2026-02-15 15:55:59 -0800

    Delete amplify directory

--- amplify/functions/my-function/handler.ts
+++
@@ -1,8 +0,0 @@
-export const handler = async (event: any = {}) => {
-  console.log("Event:", JSON.stringify(event));
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: "Hello from my-function!" }),
-  };
-};
